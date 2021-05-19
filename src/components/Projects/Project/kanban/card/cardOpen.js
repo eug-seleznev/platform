18661e52bfb17ad92a83ee0de488a3435f1392dd
alexdcrv <dikarevaleksandr0@gmail.com>
2021-05-19 18:19:28 +0300

    cards changes demo

--- src/components/Projects/Project/kanban/card/cardOpen.js
+++ src/components/Projects/Project/kanban/card/cardOpen.js
@@ -4,21 +4,26 @@ import styles from './card.module.css'
 import CardEditor from './components/CardEditor';
 import TaskTable from './components/TaskTable';
 import Comments from './components/Comments';
+import { useEffect } from 'react';
 
 
 
 
 
 
-const CardOpen = ({close}) => {
+const CardOpen = ({close, info}) => { 
+    const dispatch= useDispatch()
 
-
+    useEffect(()=>{
+      if(isOpen) {
+        dispatch(currentCard(info._id))
+      }
+    },[isOpen])
     
     return (
       <div className={styles.cardOpnenBackground} onClick={close}>
         <div className={styles.cardOpnenContainer} onClick={(e)=>e.stopPropagation()}>
-
-            <CardEditor></CardEditor>
+            <CardEditor info={info}></CardEditor>
             <TaskTable></TaskTable>
             <AddTask></AddTask>
             <Comments></Comments>
