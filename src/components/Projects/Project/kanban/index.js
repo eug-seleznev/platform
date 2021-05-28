f23c64d4d06ceeb90300c3275239d0053ccd81ea
alexdcrv <dikarevaleksandr0@gmail.com>
2021-05-28 12:29:56 +0300

    fix

--- src/components/Projects/Project/kanban/index.js
+++ src/components/Projects/Project/kanban/index.js
@@ -1,7 +1,7 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import { useDispatch, useSelector } from "react-redux"
 import styles from './kanban.module.css'
-import { addNewBoard, addNewCard } from "../../../../redux/actions/kanban";
+import { addBoardToChosen, addNewBoard, addNewCard } from "../../../../redux/actions/kanban";
 import { Button, ButtonText, ButtonTextLight } from "../../../../Styles/buttons";
 import { Input } from "../../../../Styles/Forms";
 import modelsCss from '../../../../Styles/modules/components/Project/models.module.css'
@@ -13,11 +13,13 @@ import { Path } from "../../../Layout/header";
 
 
 const Kanbans = ({history}) => {
-  const dispatch =useDispatch ()
+  const dispatch = useDispatch ()
   const project = useSelector(state=>state.projects.project)
-  
+  const user = useSelector(state=>state.auth.user)
   const [title, setTitle] = useState('')
-
+  // useEffect(()=>{
+  //   console.log (user)
+  // },[])
   const handleRedirect = (name) => {
     history.push(`/projects/${project.crypt}/board/${name}`)
   }
@@ -25,7 +27,10 @@ const Kanbans = ({history}) => {
     dispatch(addNewBoard(project.crypt, title))
     setTitle('')
   }
-
+  const chosenBoard =(id)=>{
+    console.log(id)
+    dispatch(addBoardToChosen(id))
+  }
     return (
       <div className={styles.start}>
         <div>
@@ -53,10 +58,13 @@ const Kanbans = ({history}) => {
               </div>
               {project && project.boards.map((el,i)=>{
                   return (
-                    
+                    <div style={{display:'flex'}}>
                       <div key={i} className={styles.main__board__item} onClick={()=>handleRedirect(el.name)}>
                         {el.name}
                       </div>
+                      <img onClick={()=>{chosenBoard(el._id)}} style={{cursor:'pointer',width:'30px', height:'30px',transform:'translate(10px, 15px)'}} src={Path+'star.png'}></img>
+                    </div>
+                      
                     
                   )
               })}
